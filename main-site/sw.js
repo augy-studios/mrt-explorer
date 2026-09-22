d69f9c9e0a34a181970ad9ba884dd099c9f512f4
Augy <ateoks@gmail.com>
2026-09-23 02:30:49 +0800

    init struct

--- main-site/sw.js
+++ main-site/sw.js
@@ -1,12 +1,12 @@
-const CACHE = "template-offline-v1";
+const CACHE = "mrtexplorer-offline-v1";
 
 const ASSETS = [
   "/",
   "/index.html",
   "/style.css",
   "/script.js",
-  "/templateicon1-192.png",
-  "/templateicon1-512.png",
+  "/MME-192.png",
+  "/MME-512.png",
   "/favicon.ico",
   "/manifest.json"
 ];
